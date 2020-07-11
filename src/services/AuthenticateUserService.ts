e6aac698dd381276ff44decba4b6f08fc15686b3
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2020-07-11 10:00:24 -0300

    Create JWT Authentication.

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AutenthicateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorret email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorret email/password combination.');
+    }
+
+    const token = sign({}, 'b8eaf54e5c113f6ee8573487defe7343', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AutenthicateUserService;
