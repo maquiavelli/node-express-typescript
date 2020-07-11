2192ecffd05acd554fde857a67c373bf55f2930c
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2020-07-11 10:36:23 -0300

    Refactoring config for JWT.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +32,11 @@ class AutenthicateUserService {
       throw new Error('Incorret email/password combination.');
     }
 
-    const token = sign({}, 'b8eaf54e5c113f6ee8573487defe7343', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
